80114bc46f0205d4d6f1a76658a45fd2692b73d4
sedwarb <sedwarb@gmail.com>
2022-11-21 21:04:31 -0500

    ejercicio java

---
+++ src/com/company/TipoDeDatos.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class TipoDeDatos {
+    public static void main(String[] args) {
+        System.out.println("Tipos de Datos en Java");
+        int entero = 1;
+        System.out.println("Int: "+entero);
+        short corto = 2;
+        System.out.println("Short: "+corto);
+        long largo = 100;
+        System.out.println("Long: "+largo);
+        double doble = 1.20d;
+        System.out.println("Doble: "+doble);
+        float flotante = 2.30f;
+        System.out.println("Float: "+flotante);
+        boolean booleano = false;
+        System.out.println("Boolean: "+booleano);
+        byte byteTipo = 0;
+        System.out.println("Byte: "+byteTipo);
+        char charTipo = 'a';
+        System.out.println("Char: "+charTipo);
+        String cadena = "cadena";
+        System.out.println("String: "+cadena);
+    }
+}
