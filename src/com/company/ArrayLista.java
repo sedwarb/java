3b368f43d5fe709e74668bf854db367902be5d22
sedwarb <sedwarb@gmail.com>
2022-11-29 23:22:50 -0500

    arraylist fin

--- src/com/company/ArrayLista.java
+++ src/com/company/ArrayLista.java
@@ -12,5 +12,19 @@ public class ArrayLista {
         System.out.println(lista);
         lista.remove("Elemento 2");
         System.out.println(lista);
+
+        //crear un array con arraylist
+        String[] arreglo = new String[lista.size()];
+        for (int i = 0; i < lista.size(); i++){
+            arreglo[i] = lista.get(i);
+        }
+        for (String pos : arreglo){
+            System.out.println(pos);
+        }
+        for (Object pos : lista.toArray()){
+            System.out.println(pos);
+        }
+
+
     }
 }
